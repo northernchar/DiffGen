190088269813a81c2796ac21afe508852d067e70
Vladislav <55162837+northernchar@users.noreply.github.com>
2022-06-21 11:42:49 +0300

    fix

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -18,6 +18,12 @@ function getStylishStatus(int $status)
     return '';
 }
 
+function isNode($node)
+{
+    return (isAssoc($node) && array_key_exists('data', $node)) ||
+    (!isAssoc($node) && is_array($node) && array_key_exists('data', $node[0]));
+}
+
 function stylish(mixed $node, string $replacer = " ", int $spacesCount = 2): string
 {
 
@@ -31,9 +37,7 @@ function stylish(mixed $node, string $replacer = " ", int $spacesCount = 2): str
         $bracketIndent = str_repeat($replacer, $indentSize - $spacesCount);
         $openBracket = array_key_exists('type', $node)  && $node['type'] === 'array' ? "[" : "{";
         $closeBracket = array_key_exists('type', $node) && $node['type'] === 'array' ? "]" : "}";
-        $isNode =
-        (isAssoc($node) && array_key_exists('data', $node)) ||
-            (!isAssoc($node) && is_array($node) && array_key_exists('data', $node[0]));
+        $isNode = isNode($node);
 
         if (!$isNode && is_array($node)) {
             $currentIndent = str_repeat($replacer, $indentSize + $spacesCount);
